aba239439df93d4a6b2fbe0124b67f31558129d0
anbu <anbuselvam.sk05@gmail.com>
2026-02-01 21:55:19 +0530

    Updated commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/Sidebar.css";
+import DashboardIcon from '@mui/icons-material/Dashboard';
 
 function Sidebar() {
   const navigate = useNavigate();
@@ -16,8 +17,8 @@ function Sidebar() {
       {/* Sidebar */}
       <div className={`sidebar ${open ? "open" : ""}`}>
         <ul>
-          <h2>TaskManage</h2>
-          <li onClick={() => navigate("/dashboard")}>Dashboard</li>
+          <h2>TaskManager</h2>
+          <li onClick={() => navigate("/dashboard")}> Dashboard</li>
           <li onClick={() => navigate("/Task")}>Tasks</li>
           <li onClick={() => navigate("/Pending")}>Pending</li>
           <li onClick={() => navigate("/")}>Logout</li>
